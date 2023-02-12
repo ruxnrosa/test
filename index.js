adfbe21936489252edf8a036831c14e3bf249960
ruxnrosa <85415541+ruxnrosa@users.noreply.github.com>
2023-02-12 16:37:55 -0300

    Update index.js

--- index.js
+++ index.js
@@ -82,7 +82,7 @@ webApp.post('/dialogflow', async (req, res) => {
     }
 });
 
-const PORT = 3000;
+const PORT = 5000;
 
 webApp.listen(PORT, () => {
     console.log(`Server is up and running at ${PORT}`);
