aebba980be682ca42f292ff033cd2e73e657d6e9
ruxnrosa <85415541+ruxnrosa@users.noreply.github.com>
2023-02-12 16:11:37 -0300

    Update index.js

--- index.js
+++ index.js
@@ -14,12 +14,12 @@ const textGeneration = async (prompt) => {
     try {
         const response = await openai.createCompletion({
             model: 'text-davinci-003',
-            prompt: `Human: ${prompt}\nAI: `,
-            temperature: 0.9,
-            max_tokens: 500,
+            prompt: 'Fala Comigo',
+            temperature: 0,
+            max_tokens: 100,
             top_p: 1,
             frequency_penalty: 0,
-            presence_penalty: 0.6,
+            presence_penalty: 0,
             stop: ['Human:', 'AI:']
         });
     
